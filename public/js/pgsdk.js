58e80b073ce7ed72399c16e375bd3e7319052038
kumara <akumar@personagraph.com>
2016-05-30 15:41:07 +0530

    changes calling techniques

--- public/js/pgsdk.js
+++ public/js/pgsdk.js
@@ -1,7 +1,7 @@
 (function () {
     if (!window._pgjssdk) {
+
         var baseUrl = 'ads.personagraph.com/ad/med/ss',
-            impBaseUrl = 'ec2-54-161-6-100.compute-1.amazonaws.com/med/tracking/impression',
             intOverlay,
             intOverlayId,
             intOverlayAdContainer,
@@ -13,31 +13,40 @@
                 device_id: 'pg-web-cookie',
                 bundle_id: window.location.host,
                 sdk: 'true',
-                ad_source:'js-sdk',
+                ad_source: 'js-sdk',
                 ad_type: 'false',
-                adv_type: 'DISPLAY'
+                adv_type: 'DISPLAY',
+                in_app: 'false'
             },
-            mandatoryParams = ['pid', 'ad_width', 'ad_height','container_id'],
+            mandatoryParams = ['pid', 'ad_width', 'ad_height'],
             acceptedParams = {
                 pid: 'pid',
                 ad_width: 'ad_width',
                 ad_height: 'ad_height',
                 ua: 'ua',
-                ad_source:'ad_source',
+                ad_source: 'ad_source',
                 device_id: 'device_id',
                 bundle_id: 'bundle_id',
                 sdk: 'sdk',
                 adv_type: 'adv_type',
-                ad_type:'ad_type'
+                ad_type: 'ad_type'
             },
             valid_ad_slots = [
+                '88X31',
                 '120X20',
+                '120X60',
+                '120X240',
+                '125X125',
+                '180X150',
                 '168X28',
                 '216X36',
+                '200X200',
+                '200X446',
                 '300X50',
                 '320X48',
                 '300X250',
-                '720X98',
+                '336X280',
+                '728X90',
                 '468X60',
                 '120X600',
                 '320X480',
@@ -53,73 +62,137 @@
                 '480X320',
                 '1024X768',
                 '1280X800',
-                '320X100'
+                '320X100',
+                '220X90',
+                '234X60',
+                '240X133',
+                '292X30',
+                '300X31',
+                '300X100',
+                '300X600',
+                '300X1050',
+                '320X100',
+                '960X90',
+                '970X66',
+                '970X250'
             ];
+        var s4 = function () {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+
+        var createCookieId = function () {
+            return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+                s4() + '-' + s4() + s4() + s4();
+        }
+
+        var createCookie = function () {
+
+            var name = '_pg-c-users';
+            var value = createCookieId();
+            var exp = 10 * 365 * 24 * 3600;
+
+            if (exp) {
+                var date = new Date();
+                date.setTime(date.getTime() + (exp * 1000));
+                var exp = "expires=" + date.toGMTString();
+            }
+            else {
+                var exp = "";
+            }
+            document.cookie = name + "=" + value + "; " + exp + ";path=/;";
+            return value;
+        }
+
+        var getCookie = function () {
+            var c_name = '_pg-c-users';
+            if (document.cookie.length > 0) {
+                c_start = document.cookie.indexOf(c_name + "=");
+                if (c_start != -1) {
+                    c_start = c_start + c_name.length + 1;
+                    c_end = document.cookie.indexOf(";", c_start);
+                    if (c_end == -1) {
+                        c_end = document.cookie.length;
+                    }
+                    return unescape(document.cookie.substring(c_start, c_end));
+                }
+            }
+            return "";
+        }
 
 
         var validateConfig = function (config) {
+
             for (var i = 0; i < mandatoryParams.length; i++) {
-
                 var k = mandatoryParams[i];
-
                 if (!config.hasOwnProperty(k) || config[k] === null || config[k].length === 0) {
                     throw "pgsdk.js: ERROR: missing required config parameter '" + k + "'"
-                } else {
-
-                }
-
-                if(isMobile()){
-                    if(!config.device_id)
-                        throw "pgsdk.js: ERROR: missing required config parameter device_id"
-                    if(!config.device_id)
-                        throw "pgsdk.js: ERROR: missing required config parameter device_id"
-                }
-
-                if(config.ad_type == 'int'){
-                    config.ad_type = true;
                 }
             }
+
+            if (isMobile()) {
+                if (!config.device_id)
+                    throw "pgsdk.js: ERROR: missing required config parameter device_id"
+                if (!config.bundle_id)
+                    throw "pgsdk.js: ERROR: missing required config parameter bundle_id"
+            }
+
             var adSlot = config.ad_width + "X" + config.ad_height;
+
             if (valid_ad_slots.indexOf(adSlot) == -1) {
                 throw "pgsdk.js: ERROR: invalid value of ad dimension";
             }
         }
 
         var getProtocol = function () {
-            var protocol = isMobile()?'http:':window.location.protocol;
+            var protocol = isMobile() ? 'http:' : window.location.protocol;
             return protocol ? protocol : 'http:'
         }
 
         var mergeDefaultParams = function (config) {
+
             for (var k in defaultParams) {
-                if(!config[k]){
+                if (!config[k]) {
                     config[k] = defaultParams[k];
                 }
             }
+
+            if (!isMobile()) {
+                var c_id = getCookie();
+                if (!c_id) {
+                    c_id = createCookie();
+                }
+                config.device_id = c_id;
+            } else {
+                config.in_app = 'true';
+            }
+
+            if (config.ad_type && config.ad_type.toLowerCase() === 'int') {
+                config.ad_type = true;
+            }
+            return config
         }
 
-        var isMobile = function() {
+        var isMobile = function () {
             var app = document.URL.indexOf('http://') === -1 && document.URL.indexOf('https://') === -1;
             if (app) {
                 return true;
             } else {
                 return false;
             }
-
         }
 
         var getQueryString = function (config) {
 
             var qry = [], k;
             for (k in config) {
-                if(k != 'ad_container_id'){
+                if (k != 'container_id') {
                     var value = config[k];
                     qry.push(encodeURIComponent(k) + "=" + encodeURIComponent(value))
                 }
             }
             qry.push("sdk-version=" + encodeURIComponent(versionString));
-            qry.push("format=html");
-            qry.push("__t=" + (new Date()).getTime());
             return qry.join("&");
 
         }
@@ -137,41 +210,13 @@
             return str;
         }
 
-        var wrapResponseInHtml = function (a) {
-            var b = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><html><head><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/></head><body style="margin: 0px;padding: 0px;background-color: black;">';
-            return b += a, b += "</body></html>"
-        }
-
-        var getResponse = function(iframe,a){
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    var e = xhttp.responseText;
-                    -1 == e.indexOf("<html") && (e = wrapResponseInHtml(e))
-                    e.replace("<body>", '<body style="margin:0px;padding:0px;">')
-                    iframe.contentWindow.document.open(), iframe.contentWindow.document.write(e), iframe.contentWindow.document.close();
-                }
-            };
-            xhttp.open("GET", a, true);
-            xhttp.send();
-        }
-
         var ad = function (ad, config) {
             if (typeof config == "object" && config != null) {
                 ad.config = config
             } else {
                 ad.config = defaultParams;
             }
-            //ad.iFrameRef.src = getReqUrl(config) + "&iframe=" + ad.iFrameRef.id;
-
-            var url = getReqUrl(config);
-
-            if(config.madhouse){
-                url = 'http://ec2-54-161-6-100.compute-1.amazonaws.com:4000/js/'+config.madhouse;
-            }
-
-            var response = getResponse(ad.iFrameRef,url);
-
+            ad.iFrameRef.src = getReqUrl(config) + "&iframe=" + ad.iFrameRef.id;
             ad.iFrameRef.style.display = "block";
             ad.iFrameRef.style.width = ad.iFrameRef.parentNode.style.width = config.ad_width + "px";
             ad.iFrameRef.style.height = ad.iFrameRef.parentNode.style.height = config.ad_height + "px";
@@ -188,18 +233,10 @@
             }
         }
 
-        var trackImpression = function(config){
-            var url = getProtocol()+'//'+impBaseUrl+'?'+getQueryString(config);
-            var img = document.createElement("img");
-            img.src = url;
-        }
-
-        var adCloseButton = function(iframe,config){
+        var adCloseButton = function (iframe, config) {
             var closeButton = document.createElement("div");
             closeButton.setAttribute("class", "pg-sdk-close-btn");
-            var wid = (parseInt(config.ad_width) - 42);
-            wid = (wid > 0 ? wid : 0) + "px";
-            closeButton.setAttribute("style", "position: absolute; margin-top:10px; margin-left:" + wid + ";");
+            closeButton.setAttribute("style", "position: fixed; top:10px; right:10px");
             closeButton.innerHTML = "<img src='http://ec2-54-198-185-45.compute-1.amazonaws.com/sdk/closeIcon.png' width='40px' height='40px' onclick='window._pgjssdk.closeAd(this);' />";
             iframe.parentNode.insertBefore(closeButton, iframe.parentNode.firstChild)
         }
@@ -207,46 +244,48 @@
         var initAd = function (config) {
 
             var randomId = getRandomId();
-            var iFrameID = 'pg-ifame-'+randomId, iframe, ad;
+            var iFrameID = 'pg-ifame-' + randomId, iframe, ad;
             var iframe = document.createElement("iframe");
             var adContainer = document.createElement("div");
 
-            console.log(intOverlay);
-            if(config.ad_type === 'int' || config.ad_type == true ){
+            if (config.ad_type === 'int' || config.ad_type == true) {
 
-                if(!intOverlay){
+                if (!intOverlay) {
                     adContainer.style.position = 'absolute';
                     adContainer.style.top = "0px";
-                    var marginLeft = Math.ceil((window.innerWidth - config.ad_width)/2);
-                    marginLeft = marginLeft > 0?marginLeft:0;
-                    var marginTop = Math.ceil((window.innerHeight - config.ad_height)/2);
-                    marginTop = marginTop > 0?marginTop:0;
-                    adContainer.style.marginLeft=marginLeft+'px';
-                    adContainer.style.marginTop=marginTop+'px';
+                    adContainer.scrolling = "no";
+                    var marginLeft = Math.ceil((window.innerWidth - config.ad_width) / 2);
+                    marginLeft = marginLeft > 0 ? marginLeft : 0;
+                    var marginTop = Math.ceil((window.innerHeight - config.ad_height) / 2);
+                    marginTop = marginTop > 0 ? marginTop : 0;
+                    adContainer.style.marginLeft = marginLeft + 'px';
+                    adContainer.style.marginTop = marginTop + 'px';
                     document.body.appendChild(adContainer);
                     intOverlay = document.createElement("div");
-                    intOverlayId = 'pg-int-overlay-'+randomId;
+                    intOverlayId = 'pg-int-overlay-' + randomId;
                     intOverlay.id = intOverlayId;
                     intOverlay.style.position = 'absolute';
                     intOverlay.display = 'block';
                     intOverlay.style.top = "0px";
-                    intOverlay.style.background= "#999";
+                    intOverlay.style.background = "#999";
                     intOverlay.style.opacity = 0.5;
-                    intOverlay.style.zIndex=999;
-                    intOverlay.style.width="100%";
-                    intOverlay.style.height="100%";
-                    adContainer.style.zIndex = 10000;
+                    intOverlay.style.zIndex = 6000001;
+                    intOverlay.style.width = "100%";
+                    intOverlay.style.height = document.body.clientHeight + 'px';
+                    intOverlay.scrolling = "no";
+                    adContainer.style.zIndex = 6000002;
+
                 }
 
-            }else{
-                var scriptContainer = document.getElementById(config.container_id);
-                if(scriptContainer){
-                    scriptContainer.parentNode.insertBefore(adContainer,scriptContainer);
+            } else {
+                var scriptContainer = document.getElementById('_pgads');
+                if (scriptContainer) {
+                    scriptContainer.parentNode.insertBefore(adContainer, scriptContainer);
                 }
             }
 
             iframe.class = "pg-sdk-ad";
-            iframe.id= iFrameID;
+            iframe.id = iFrameID;
             iframe.style.width = config.ad_width;
             iframe.style.height = config.ad_height;
             iframe.style.border = "none";
@@ -254,13 +293,11 @@
             iframe.scrolling = "no";
 
             adContainer.appendChild(iframe);
-
             iframe.onload = function (event) {
-                if(config.ad_type == 'int' || config.ad_type == true){
-                    adCloseButton(this,config);
+                if (config.ad_type == 'int' || config.ad_type == true) {
+                    adCloseButton(this, config);
                     document.body.appendChild(intOverlay);
                 }
-                trackImpression(config);
             }
             ad = createAd(iframe, config);
             ad.getNewAd(config);
@@ -269,25 +306,26 @@
         window._pgjssdk = {
 
             showAd: function (config, cb) {
-                setTimeout(function () {
-                    validateConfig(config)
-                    mergeDefaultParams(config);
+               setTimeout(function () {
+                    validateConfig(config);
+                    config = mergeDefaultParams(config);
                     initAd(config);
-                }, 0);
+               }, 0);
             },
 
-            closeAd:function(ele){
+            closeAd: function (ele) {
                 var rmEl = ele.parentNode.parentNode;
                 if (rmEl) {
                     ele.parentNode.parentNode.parentNode.removeChild(rmEl)
                 }
-                if(intOverlay){
+                if (intOverlay) {
                     document.body.removeChild(intOverlay);
                 }
             }
         }
 
-    }else{
+    } else {
         console.log('File was already included');
     }
+
 })();
